6334489c63411420db3017e87b572012c9dc84e3
Jonathan Boylan <jtylerboylan@outlook.com>
2024-03-29 12:03:28 -0400

    Jetson docker

---
+++ run_jetson.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+xhost +local:root
+
+PROJECT_NAME=starq
+PROJECT_DIR=/home/nvidia/starq_ws/src/
+
+# Get the absolute path to the directory containing this script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Build the Docker image
+docker build -t ${PROJECT_NAME}:jetson -f "${SCRIPT_DIR}/Dockerfile.jetson" "${SCRIPT_DIR}"
+
+# Start the Docker container
+docker run -it \
+    --rm \
+    --net host \
+    --runtime nvidia \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v "/${SCRIPT_DIR}:${PROJECT_DIR}" \
+    ${PROJECT_NAME}:jetson \
+    bash
