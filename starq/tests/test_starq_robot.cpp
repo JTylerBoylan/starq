5b8133a848a2cad4b796a71aed7e6381209096c9
Jonathan Boylan <jtylerboylan@outlook.com>
2024-05-02 12:03:10 -0400

    test stand

--- starq/tests/test_starq_robot.cpp
+++ starq/tests/test_starq_robot.cpp
@@ -27,27 +27,44 @@ int main()
         STARQ->setFootPosition(i, center_position);
     }
 
-    // Wait for 5 seconds
-    usleep(5E6);
-
-    // Move legs in a circle
-    const float frequency = 0.5f;
-    const int resolution = 100;
-    const int num_cycles = 3;
-    const float radius = 0.025f;
-    for (int i = 0; i < num_cycles; i++)
+    const int time_s = 30;
+    for (int t = 0; t < time_s; t++)
     {
-        for (int j = 0; j < resolution; j++)
+        printf("-------------------------\n");
+        printf("Time: %d s\n", t);
+        for (auto motor : STARQ->getMotors())
         {
-            const float angle = 2.0f * M_PI * j / resolution;
-            const Vector3 circle_position(radius * cos(angle), 0, radius * sin(angle));
-            STARQ->setFootPosition(0, center_position + circle_position);
-            STARQ->setFootPosition(1, center_position + circle_position);
-            STARQ->setFootPosition(2, center_position + circle_position);
-            STARQ->setFootPosition(3, center_position + circle_position);
-            usleep(1E6 / frequency / resolution);
+            // Cast as odrive motor
+            auto odrive_motor = std::dynamic_pointer_cast<odrive::ODriveController>(motor);
+
+            // Get odrive temperature
+            const int id = odrive_motor->getCANID();
+            const Float temp = odrive_motor->getFETTemperature();
+
+            printf("[Motor %d] FET Temp: %.2f\n", id, temp);
         }
+        printf("-------------------------\n");
+        usleep(1E6);
     }
+
+    // // Move legs in a circle
+    // const float frequency = 0.5f;
+    // const int resolution = 100;
+    // const int num_cycles = 3;
+    // const float radius = 0.025f;
+    // for (int i = 0; i < num_cycles; i++)
+    // {
+    //     for (int j = 0; j < resolution; j++)
+    //     {
+    //         const float angle = 2.0f * M_PI * j / resolution;
+    //         const Vector3 circle_position(radius * cos(angle), 0, radius * sin(angle));
+    //         STARQ->setFootPosition(0, center_position + circle_position);
+    //         STARQ->setFootPosition(1, center_position + circle_position);
+    //         STARQ->setFootPosition(2, center_position + circle_position);
+    //         STARQ->setFootPosition(3, center_position + circle_position);
+    //         usleep(1E6 / frequency / resolution);
+    //     }
+    // }
 
     // // Load trajectory from file
     // STARQ->loadTrajectory("/home/nvidia/starq_ws/src/starq/trajectories/walk_test.txt");
